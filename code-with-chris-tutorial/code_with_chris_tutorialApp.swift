b77239d7bf4230d36f6301a0f9fc4f0a5216414d
Kareha Agesa <kmaagesa@gmail.com>
2021-01-25 13:19:49 -0800

    Initial Commit

---
+++ code-with-chris-tutorial/code_with_chris_tutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  code_with_chris_tutorialApp.swift
+//  code-with-chris-tutorial
+//
+//  Created by Kareha on 1/25/21.
+//
+
+import SwiftUI
+
+@main
+struct code_with_chris_tutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
